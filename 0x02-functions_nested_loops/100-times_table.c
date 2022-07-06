a6625afec63bbe34ec137c3bfe31d1be64f52d3b
evuru <evurustanle@gmail.com>
2022-07-05 20:39:20 -0700

    I'm not used to betty style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,23 +25,20 @@ void print_times_table(int n)
 				}
 				else if (c < 10)
 				{
-					_putchar(' '); _putchar(' ');
+					_putchar(' '), _putchar(' ');
 					_putchar('0' + f);
 				}
 				else if (c < 100)
 				{
-					_putchar(' ');
-					_putchar('0' + e); _putchar('0' + f);
+					_putchar(' '), _putchar('0' + e), _putchar('0' + f);
 				}
 				else
 				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
+					_putchar('0' + d), _putchar('0' + e), _putchar('0' + f);
 				}
 				if (b < n)
 				{
-					_putchar(','); _putchar(' ');
+					_putchar(','), _putchar(' ');
 				}
 				else
 					_putchar('\n');
