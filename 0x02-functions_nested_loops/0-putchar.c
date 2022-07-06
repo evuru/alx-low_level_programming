192470025415ea7ed22b487690c8e92e45c0bc6e
evuru <evurustanle@gmail.com>
2022-07-05 20:26:48 -0700

    My code was not betty style hence the error from 0-putchar \nhope it works now,also corrected the main.h for putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,23 +3,22 @@
 /**
  * main - entry point
  *
- * Description: Prints putchar with _putchar
+ * Description: Prints Holberton with _putchar
  *
  * Return: always 0
  */
 
 int main(void)
 {
-	int i = 0;
-	char c;
-	char s[] = "_putchar\n";
-
-	while (i <= 9)
-	{
-		c = s[i];
-		_putchar(c);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
 
