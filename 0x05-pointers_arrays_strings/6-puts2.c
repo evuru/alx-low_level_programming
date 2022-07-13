a7c0604dbb343f4e5f3e99b730ea6333eef1af08
evuru <evurustanle@gmail.com>
2022-07-12 20:00:08 -0700

    making corrections

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if(1 % 2 == 0){
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
