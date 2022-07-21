94eb6c1acb9ffe6dd08052cd26055ce18476989e
evuru <evurustanle@gmail.com>
2022-07-20 17:33:03 -0700

    Recursion in c restructuredis palindrome seems to be stuck in the checker

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,48 +1,66 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
+int find_pal(char *s, int f_i, int b_i);
+int count_string(char *s);
 /**
- * _strlen - return length of string
- * @str: string to check
+ * is_palindrome - checks if string is a palindrom
+ * @s: string to check.
  *
- * Return: length of str
- */
-int _strlen(char *str)
-{
-	if (*str == '\0')
-		return (0);
-	else
-		return (1 + _strlen(str + 1));
-}
-
-/**
- * check_palindrome - checks to see if a string is a palindrome
- * @l: left hand index
- * @r: right hand index
- * @p: possible palindrome
- *
- * Return: 1 if palindrome 0 if not
- */
-int check_palindrome(int l, int r, char *p)
-{
-	if (l >= r)
-		return (1);
-	else if (p[l] != p[r])
-		return (0);
-	else
-		return (check_palindrome(l + 1, r - 1, p));
-}
-
-/**
- * is_palindrome - states if a string is a palindrome
- * @s: string to check
- *
- * Return: 1 if palindrome, 0 if not
+ * Return: 1 if a string is a palindrome and 0 if not.
  */
 int is_palindrome(char *s)
 {
-	int i;
+	int count;
 
-	i = _strlen(s) - 1;
-	return (check_palindrome(0, i, s));
+	count = 0;
+	if (!(*s))
+		return (1);
+
+	count = count_string(s);
+	return (find_pal(s, 0, count - 1));
 }
+
+/**
+ * find_pal - helper for is_palindrom
+ * @s: string to check
+ * @f_i: front index.
+ * @b_i: back index.
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+int find_pal(char *s, int f_i, int b_i)
+{
+	/*printf("%c - %c : %d - %d\n", s[f_i], s[b_i], f_i, b_i); */
+	if (s[f_i] != s[b_i])
+		return (0);
+	if (s[f_i] == s[b_i] && f_i == b_i)
+		return (1);
+	else if (s[f_i] == s[b_i] && f_i > b_i)
+		return (1);
+	else
+		return (find_pal(s, f_i + 1, b_i - 1));
+}
+
+/**
+ * count_string - count the string.
+ * @s: string to count.
+ *
+ * Return: Int value of the length.
+ */
+int count_string(char *s)
+{
+	if (*s)
+		return (count_string((s + 1)) + 1);
+	return (0);
+}
+
+
+
+
+
+
+
+
+
 
